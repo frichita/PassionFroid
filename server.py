ff3df243cad64591d2972aecbb26839e4879eee4
afardous <afardous98@gmail.com>
2023-08-26 19:25:31 +0200

    tags et meta description mais supression pas au point

--- server.py
+++ server.py
@@ -4,7 +4,11 @@ from flask_pymongo import PyMongo
 from flask import render_template
 from flask_cors import CORS
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
 import os
+import io
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
@@ -14,6 +18,11 @@ ACCOUNT_KEY = 'HuL6tfRJysTzKJDgo0YG/ML6M1UYJMdmFUEHUEvobVrB6SesnqJPkvAfq/6W7/mN+
 CONTAINER_NAME = 'passion-froid'
 BLOB_SERVICE = BlobServiceClient(account_url=f"https://{ACCOUNT_NAME}.blob.core.windows.net", credential=ACCOUNT_KEY)
 CONTAINER_CLIENT = BLOB_SERVICE.get_container_client(CONTAINER_NAME)
+
+SUBSCRIPTION_KEY = "472be19f-4437-4866-9a98-938aa71fd15e"
+ENDPOINT = "aacfabf84826411baf568cbad2c51c4a"
+
+computervision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(SUBSCRIPTION_KEY))
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/passionFroid"
 mongo = PyMongo(app)
@@ -25,10 +34,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def show_images():
     # Récupérer les noms de tous les blobs dans le conteneur
     blob_list = CONTAINER_CLIENT.list_blobs()
-    image_urls = [f"https://{ACCOUNT_NAME}.blob.core.windows.net/{CONTAINER_NAME}/{blob.name}" for blob in blob_list]
+    blob_urls = [f"https://{ACCOUNT_NAME}.blob.core.windows.net/{CONTAINER_NAME}/{blob.name}" for blob in blob_list]
+
+    # Récupérer les métadonnées depuis MongoDB
+    images_data = mongo.db.images.find({"image_path": {"$in": blob_urls}})
+    
+    image_list = []
+
+    for image_data in images_data:
+        image_dict = {
+            "image_path": image_data['image_path'],
+            "tags": image_data.get('tags', []),
+            "meta_description": image_data.get('meta_description', '')
+        }
+        image_list.append(image_dict)
 
     # Retourner une réponse JSON
-    return jsonify({'image_urls': image_urls})
+    return jsonify({'images': image_list})
 
 @app.route('/update/<blob_name>', methods=['POST'])
 def update_image(blob_name):
@@ -83,23 +105,34 @@ def upload_image():
     if blob_client.exists():
         return jsonify({'error': 'Image already exists'}), 409
 
-    # Save the image
-    image_path = os.path.join(UPLOAD_FOLDER, image_filename)
-    image.save(image_path)
+    # Read image into memory
+    image_bytes = image.read()
 
     # Upload to Azure Blob Storage
-    with open(image_path, "rb") as data:
-        blob_client.upload_blob(data)
+    blob_client.upload_blob(image_bytes)
     blob_url = blob_client.url
     
-    # Delete local image after upload
-    os.remove(image_path)
+    # Initialize Azure Cognitive Services client
+    computervision_client = ComputerVisionClient(
+        "https://passion-froid-vision.cognitiveservices.azure.com/",
+        CognitiveServicesCredentials("aacfabf84826411baf568cbad2c51c4a")
+    )
+    
+    # Analyze image using Azure Computer Vision API
+    image_analysis = computervision_client.analyze_image_in_stream(
+        io.BytesIO(image_bytes),
+        visual_features=[VisualFeatureTypes.tags, VisualFeatureTypes.description]
+    )
+    
+    tags = [tag.name for tag in image_analysis.tags]
+    description = image_analysis.description.captions[0].text if image_analysis.description.captions else ""
 
     # Insert data into MongoDB
     data = {
-        'image_path': blob_url,  # Use the blob URL instead of the local path
+        'image_path': blob_url,
         'other_field': other_field,
-        'meta_description': other_field  # Adding the "other_field" to meta description
+        'tags': tags,  # New field for tags
+        'meta_description': description  # Updated with generated description
     }
     mongo.db.images.insert_one(data)
 
